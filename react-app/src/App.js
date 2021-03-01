afa5d12ee09c9c49730aa27c777ebe90c4ba4287
whelxks <70426064+whelxks@users.noreply.github.com>
2021-03-01 12:55:33 +0800

    Updated with table for audits

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,18 @@
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import DashboardStaff from './views/DashboardStaff';
-import AuditsStaff from './views/AuditsStaff';
-import TenantStaff from './views/TenantStaff';
-import ReportsStaff from './views/ReportsStaff';
-import React, { Component } from 'react';
-import Sidebar from './components/Sidebar';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import DashboardStaff from "./views/DashboardStaff";
+import AuditsStaff from "./views/AuditsStaff";
+import TenantStaff from "./views/TenantStaff";
+import ReportsStaff from "./views/ReportsStaff";
+import Sidebar from "./components/Sidebar";
 
 class App extends Component {
-  render() { 
-    return ( 
-      <div>
+  render() {
+    return (
+      <BrowserRouter>
         <div className="content">
+          
           <Switch>
             {/* Route is a wrapper around the component. It contains history, location and match (info about URL) */}
             <Route path="/dashboard-staff" component={DashboardStaff} />
@@ -22,8 +23,9 @@ class App extends Component {
             <Route path="/" exact component={DashboardStaff} />
           </Switch>
         </div>
-      </div> );
+      </BrowserRouter>
+    );
   }
 }
- 
+
 export default App;
--- react-app/src/views/AuditsStaff.js
+++ react-app/src/views/AuditsStaff.js
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Sidebar from "../components/Sidebar";
+import Table from "../components/Table";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 
 class AuditStaff extends Component {
-    render() { 
-        return ( <h1 className="header-style" >Audits for staff goes here</h1> );
-    }
+  render() {
+    return (
+      //side bar
+      //button to filter ongoing and resolved audits
+      //button to filter last added, last checked, alphabetical order tenants name
+      //Table
+      //button to saved audits page
+      //button to create new audit page
+
+      <div>
+        <Tabs defaultActiveKey="profile">
+          <Tab eventKey="Ongoing" title="Ongoing Audits"></Tab>
+          <Tab eventKey="Resolved" title="Resolved Audits"></Tab>
+        </Tabs>
+
+        <Sidebar />
+        <Table />
+      </div>
+    );
+  }
 }
- 
+
 export default AuditStaff;
