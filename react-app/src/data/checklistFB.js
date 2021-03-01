ad8f95ae4fabd3aba06e391cd075c7693844379e
radjsh <arissa140100@yahoo.com.sg>
2021-03-01 14:27:47 +0800

    added checkbox

--- react-app/src/data/checklistFB.js
+++ react-app/src/data/checklistFB.js
@@ -1,22 +1,26 @@
 const checklistFB = [
     {
         id: "professionalism_01",
+        category: "professionalism",
         item: "Shop is open and ready to service patients / visitors according to operating hours",
         checked: 0,
-        noncompliance: "false"
+        noncompliance: false
     },
     {
         id: "professionalism_02",
+        category: "professionalism",
         item: "Staff Attendance: Adequate staff for peak and non-peak hours",
         checked: 0,
-        noncompliance: "false"
+        noncompliance: false
     },
     {
         id: "professionalism_03",
+        category: "professionalism",
         item: "At least one (1) clearly assigned person in-charge on site",
         checked: 0,
-        noncompliance: "false"
+        noncompliance: false
     }
+
 ]
 
 export function getAllChecklistItems(){
