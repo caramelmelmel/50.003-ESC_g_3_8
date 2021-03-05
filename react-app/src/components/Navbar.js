f7972e6ed08cf7cd5df8d337a87b82c90c5be925
whelxks <70426064+whelxks@users.noreply.github.com>
2021-03-05 15:17:27 +0800

    Navbar functionality works now
    thanks peeps:)

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -9,13 +9,14 @@ import { IconContext } from "react-icons";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
+
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <>
-      <IconContext.Provider value={{ color: "#fff" }}>
+      <IconContext.Provider value={{ color: "black" }}>
         <div className="navbar">
           <Link to="#" className='menu-bars'>
             <FaIcons.FaBars onClick={showSidebar} />
