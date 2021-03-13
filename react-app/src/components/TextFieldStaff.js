57a173485a9ba8a757980dfc2a53bc0a023b72ad
geraldhyw <62137503+geraldhyw@users.noreply.github.com>
2021-03-14 00:52:12 +0800

    updated links in handleCreateAudit()

--- react-app/src/components/TextFieldStaff.js
+++ react-app/src/components/TextFieldStaff.js
@@ -53,12 +53,11 @@ class TextFieldStaff extends Component {
         this.setState({clicked: true});
         if (this.state.selectedCategory === "F&B" && this.state.checkAll === true) {
             console.log("going to f&b checklist");
-
-            this.props.history.push("/checklist-fb-staff");
+            this.props.history.push("/checklist-fb-staff-professionalism-and-staff-hygiene");
         } 
-      else if (this.state.selectedCategory === "Non-F&B" && this.state.checkAll === true){
-            
-          this.props.history.push("/checklist-fb-staff-professionalism-and-staff-hygiene");
+        else if (this.state.selectedCategory === "Non-F&B" && this.state.checkAll === true){
+            console.log("going to non f&b checklist");
+            this.props.history.push("/checklist-non-fb-staff");
         } 
     }
         
