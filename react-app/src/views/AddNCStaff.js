7539a795bafe1a296481da70dae94b19938240cd
whelxks <70426064+whelxks@users.noreply.github.com>
2021-03-25 18:05:40 +0800

    Implemented take photo & preview function
    photo & additional comments still not saved to local storage

--- react-app/src/views/AddNCStaff.js
+++ react-app/src/views/AddNCStaff.js
@@ -1,92 +1,176 @@
 import React, { Component } from "react";
-import '../index.css'
-import { Container, Row, Col, Form } from "react-bootstrap";
-import { getAllChecklistItems, getChecklistItem } from './../services/checklistFB';
+import "../index.css";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import {
+  getAllChecklistItems,
+  getChecklistItem,
+} from "./../services/checklistFB";
 import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
+import Webcam from "react-webcam";
+import ImagePreview from "../components/ImagePreview";
 
+import Camera from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
 
 class AddNCStaff extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      val: "",
+      dataUri: null,
+      checklistItem: getChecklistItem(props.location.state.itemId),
+      itemName: props.itemName,
+      clickedItems: props.clickedItems,
+    };
+    this.onTakePhoto = this.onTakePhoto.bind(this);
+
+    //this.handleSave = this.handleSave.bind(this);
+
+    //this.textInput = React.createRef();
+  }
+
+  /*
   state = {
     checklistItem: getChecklistItem(this.props.location.state.itemId),
     itemName: this.props.itemName,
-    clickedItems: this.props.clickedItems
-  };
+    clickedItems: this.props.clickedItems,
+    
+    
+
+  };*/
 
   // TO ROUTE BACK TO PREVIOUS PAGE IF CANCELLED IS BEING CALLED
   static contextTypes = {
-    router: PropTypes.object, 
+    router: PropTypes.object,
   };
 
-  handleUploadImage() {
-    console.log("Upload Image Button Clicked");
+  componentWillUnmount() {
+    // fix Warning: Can't perform a React state update on an unmounted component
+    this.setState = (state, callback) => {
+      return;
+    };
   }
 
-  handleSave(){
-    console.log("Saving...")
-    
+  onTakePhoto(dataUri) {
+    // Do stuff with the dataUri photo...
+    console.log("takePhoto");
+    this.setState({ dataUri });
   }
 
-  handleCancel(){
-    console.log("Cancelling...")
+  handleChange() {}
+
+  handleSave(event, result) {
+    console.log("Saving...");
+    //alert("Text field value is: " + this.state.va);
+  }
+
+  handleCancel() {
+    console.log("Cancelling...");
   }
 
   render() {
-    console.log("NC recieved itemId: ", this.props.location.state);
-    console.log("Checklist Item: ", this.state.checklistItem);
-    console.log("Props: ", this.props);
+    //console.log("NC recieved itemId: ", this.props.location.state);
+    //console.log("Checklist Item: ", this.state.checklistItem);
+    //console.log("Props: ", this.props);
+    //console.log(this.state.dataUri); //check output of image format
+
     const { itemsChecked } = this.props;
     //console.log("CLICKED ITEMS PASSED: ", this.props.location.state.clickedItems);
 
-    return (<React.Fragment>
-      <Container fluid>
-        <Row>
-          <Col xs={2} id="sidebar-wrapper">      
-            {/*<Sidebar />*/}
-          </Col>
-          <Col xs={10}>
-            <Row>
-              <Col className="non-compliance-header" xs={12}>Add Non-Compliance</Col>
-            </Row>
-            <Row className="lg">
-              <Col className="checklist-header-style">{this.state.checklistItem.item}
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={10} className="mt-5">
-                <button className="btn btn-block btn-lg btn-outline-dark checklist-sideheader-style" 
-                onClick={this.handleUploadImage}
-                style={{ marginLeft: "5%", marginRight: "0%", float: "left", width: "80%"}}>Upload Image</button>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={10} className="mt-5">
-              <Form.Control
-                as="textarea"
-                placeholder="Additional comments"
-                rows={2}
-                style={{ marginLeft: "5%", marginRight: "0%", float: "left", width: "80%"}}
-              />
-              </Col>
-            </Row>
-              <Link to={{pathname: `/`, state: {itemId: this.state.checklistItem.id}}} >
-                <button 
-                className="btn btn-lg btn-warning checklist-sideheader-style mt-5"
-                style={{float: 'right'}} 
-                onClick={this.context.router.history.goBack}>Submit</button> {/* Now it's same as cancel, need to change this */}
+    return (
+      <React.Fragment>
+        <Container fluid>
+          <Row>
+            <Col xs={2} id="sidebar-wrapper">
+              {/*<Sidebar />*/}
+            </Col>
+            <Col xs={10}>
+              <Row>
+                <Col className="non-compliance-header" xs={12}>
+                  Add Non-Compliance
+                </Col>
+              </Row>
+              <Row className="lg">
+                <Col className="checklist-header-style">
+                  {this.state.checklistItem.item}
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={10} className="mt-5">
+                  <div className="camera">
+                    {this.state.dataUri ? (
+                      <ImagePreview dataUri={this.state.dataUri} />
+                    ) : (
+                      <Camera onTakePhoto={this.onTakePhoto} />
+                    )}
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={10} className="mt-5">
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Additional comments"
+                    rows={2}
+                    style={{
+                      marginLeft: "0%",
+                      marginRight: "0%",
+                      marginTop: "5%",
+                      float: "left",
+                      width: "80%",
+                    }}
+                    val={this.state.val}
+                    ref={this.textInput}
+                    type="text"
+                    onChange={(val) => this.setState({ val })}
+                  />
+                </Col>
+              </Row>
+              <Link
+                to={{
+                  pathname: `/`,
+                  state: {
+                    itemId: this.state.checklistItem.id,
+                    val: this.state.val,
+                  },
+                }}
+              >
+                <button
+                  className="btn btn-lg btn-warning checklist-sideheader-style mt-5"
+                  style={{ float: "right" }}
+                  onClick={this.handleSave}
+                >
+                  Submit
+                </button>{" "}
+                {/* Now it's same as cancel, need to change this */}
               </Link>
-                <button 
-                  className="btn btn-lg btn-danger checklist-sideheader-style mt-5"
-                  style={{float: 'left'}} 
-                  onClick={this.context.router.history.goBack}
-                  >Cancel</button>
-          </Col>
-        </Row>
-      </Container>
-    </React.Fragment>
+              <button
+                className="btn btn-lg btn-danger checklist-sideheader-style mt-5"
+                style={{ float: "left" }}
+                onClick={this.context.router.history.goBack}
+              >
+                Cancel
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
     );
   }
 }
 
 export default AddNCStaff;
+//<WebcamCapture /> <Button onClick={this.handleUploadImage} style={{width: 300}}>
+//Upload Image
+//<Button onClick={this.handleUploadImage} style={{width: 300, marginTop:"0%"}}>Capture photo</Button>
+//</Button>
+
+/*
+<WebcamCapture/>
+                  <img src={WebcamCapture.imgSrc}/>   
+
+onChange={e => this.setState({ val: e.target.value })}
+onClick={this.context.router.history.goBack, 
+*/
