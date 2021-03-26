c215a2a333828551fd776f18a4c05516286d173d
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-03-26 12:02:17 +0800

    gerald added login, register and tests

--- react-app/src/components/RegisterStaff.js
+++ react-app/src/components/RegisterStaff.js
@@ -1,22 +1,86 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
+import { getAllTenantInfo } from './../data/tenantInfo';
+
+// must have "@singhealth.com.sg" and be a word infront
+var regexEmail = /^\w{0,}@singhealth\.com\.sg$/;
+// one uppercase + lowercase + num + symb, min 8 char
+var regexPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 
 class LoginStaff extends Component {
     state = {
+        institution: "",
         name: "",
         email: "",
         password: "",
         isClicked: false,
+        isInvalid: false,
         error: "Fill in all fields!",
+        info: getAllTenantInfo(),
+        selectedInstitution: "Select Institution",
      }
-
+     
     handleSubmit = () => {
+        console.log(this.state.institution);
         console.log(this.state.name);
         console.log(this.state.email);
         console.log(this.state.password);
+        // console.log(this.state.error);
+        // console.log(this.state.isClicked);
         this.setState({isClicked: true});
+
+        // console.log(regexEmail.test(this.state.email));
+        // console.log(regexPassword.test(this.state.password));
+
+        var objString = {
+            staff_institution: this.state.institution,
+            staff_name: this.state.name,
+            staff_email: this.state.email,
+            staff_password: this.state.password,
+        }
+
+        var JSONdata = JSON.parse(objString);
+
+        var isEmail = regexEmail.test(this.state.email);
+        var isPassword = regexPassword.test(this.state.password);
+
+        if (isEmail === false && isPassword === false) {
+            this.setState({error: "Invalid email and password given."});
+            this.setState({isInvalid: true});
+        } else if (isPassword === false) {
+            this.setState({error: "Invalid password given."});
+            this.setState({isInvalid: true});
+        } else if (isEmail === false) {
+            this.setState({error: "Invalid email given."});
+            this.setState({isInvalid: true});
+        } else {
+            this.setState({error: "Fill in all fields!"});
+            this.setState({isInvalid: false});
+            if (this.state.institution != "" && this.state.name != "" && this.state.email != "" && this.state.password != "") {
+                // send data to db
+                this.createStaff(JSONdata);
+            }
+        }
     }
-    
+
+    // synchronous call to create staff
+    createStaff(data) {
+        try {
+            // not sure about host here
+            fetch("http://localhost:5000/staffreg", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+              },
+            body: data,
+            })
+            console.log("Staff created");
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
     render() { 
         const marginVertSpace = 5;
         const titleStyle = {
@@ -46,6 +110,23 @@ class LoginStaff extends Component {
             border: "2px solid black",
             width: "100%",
         }
+        const buttonStyle = {
+            margin: "auto",
+            marginTop: marginVertSpace,
+            textAlign: "left",
+            borderRadius: 3,
+            border: "0.1px solid black",
+            width: "90%",
+        }
+        const toggleStyle = {
+            margin: "auto",
+            textAlign: "left",
+            textColor: "black",
+            borderRadius: 3,
+            border: "0.1px solid black",
+            width: "100%",
+            color: "black",
+        }
         const errorStyle = {
             float: "left",
             marginTop: marginVertSpace + 10,
@@ -73,9 +154,31 @@ class LoginStaff extends Component {
             border: "0px solid white",
         }
         return <React.Fragment>
-            <div style={titleStyle}>Staff Login</div>
+            <div style={titleStyle}>Staff Registration</div>
 
             <Form>
+
+                {/* INSTITUTION */}
+                <div style={headerStyle}>Institution</div>
+
+                <Dropdown
+                style={buttonStyle}>
+
+                    <Dropdown.Toggle 
+                    variant=""
+                    style={toggleStyle}>
+                        {this.state.selectedInstitution}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu
+                    style={{width: "100%", margin: "auto"}}>
+                        {this.state.info.institutions.map(option => 
+                            (<Dropdown.Item
+                            onSelect={() => this.handleInstitutionChange({option})}>
+                                { option }
+                            </Dropdown.Item>))}
+                    </Dropdown.Menu>
+                </Dropdown>
 
                 {/* NAME */}
                 <Form.Group 
@@ -120,19 +223,19 @@ class LoginStaff extends Component {
                 </Form.Group>
 
                 {/* ERROR MESSAGE */}
-                {(this.state.name == "" || this.state.email == "" || this.state.password == "") && this.state.isClicked == true ? 
+                {((this.state.institution == "" || this.state.name == "" || this.state.email == "" || this.state.password == "") && this.state.isClicked == true) || this.state.isInvalid == true ? 
                 <div
                 style={errorStyle}>
                     {this.state.error}
                 </div> : <div></div>
                 }
                 
-                {/* LOGIN */}
+                {/* REGISTER */}
                 <button 
                 type="button"
                 style={submitStyle}
                 onClick={this.handleSubmit}>
-                    Login
+                    Register
                 </button>
             </Form>
         </React.Fragment>
